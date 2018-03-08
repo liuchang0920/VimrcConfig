4b55a2b10f877564f6495f011e25b5e3729f7e69
chang liu <cl3869@nyu.edu>
2018-03-08 02:57:13 -0500

    Update setup-software.sh

--- setup-software.sh
+++ setup-software.sh
@@ -5,6 +5,6 @@ sudo apt-get install vim
 sudo apt-get install git
 sudo apt-get install powertop
 
-curl https://bootstrap.pypa.io/get-pip.py
-sudo python get-pip.py
+wget https://bootstrap.pypa.io/get-pip.py
+sudo python3 get-pip.py
 sudo python3 -m pip install numpy scipy matplotlib pandas
