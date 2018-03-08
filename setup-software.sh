defd73278e793393f70ac825ad7ed39080f18fbb
chang liu <cl3869@nyu.edu>
2018-03-08 03:11:39 -0500

    Update setup-software.sh

--- setup-software.sh
+++ setup-software.sh
@@ -6,9 +6,16 @@ sudo apt-get install git
 sudo apt-get install powertop
 
 sudo apt-get install unity-tweak-tool
+#theme
 sudo apt-add-repository ppa:tista/adapta
 sudo apt-get update && sudo apt-get install adapta-gtk-theme
-
+sudo add-apt-repository ppa:noobslab/themes
+sudo apt-get update
+sudo apt-get install flatabulous-theme
+#icon
+sudo add-apt-repository ppa:snwh/pulp
+sudo apt-get update
+sudo apt-get install paper-icon-theme
 
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
