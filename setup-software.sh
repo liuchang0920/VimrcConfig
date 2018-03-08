6d002349810dbc86082c4d4e305f936b7022744c
chang liu <cl3869@nyu.edu>
2018-03-08 03:09:31 -0500

    Update setup-software.sh

--- setup-software.sh
+++ setup-software.sh
@@ -5,6 +5,11 @@ sudo apt-get install vim
 sudo apt-get install git
 sudo apt-get install powertop
 
+sudo apt-get install unity-tweak-tool
+sudo apt-add-repository ppa:tista/adapta
+sudo apt-get update && sudo apt-get install adapta-gtk-theme
+
+
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
 sudo python3 -m pip install numpy scipy matplotlib pandas
