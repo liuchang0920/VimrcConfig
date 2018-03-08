71b382ad20218c27b2af675855abaa10318570ba
chang liu <cl3869@nyu.edu>
2018-03-08 02:51:47 -0500

    Create setup-software.sh

---
+++ setup-software.sh
@@ -0,0 +1,6 @@
+# set up some softwares when first install ubuntu etc
+
+sudo apt-get install curl
+sudo apt-get install vim
+sudo apt-get install git
+sudo python3 -m pip install numpy scipy matplotlib pandas
